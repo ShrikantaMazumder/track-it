eff3db5f78cd1551677c24c675e0c84c3cf65c6b
shrikanta <shrikanta@genuino.world>
2021-12-26 00:26:13 +0600

    routing

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ import express from "express";
 
 const app = express()
 const port = 3000
+app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.send('Hello Universe')
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+    console.log(req);
+    res.send(JSON.stringify(data))
+})
 app.listen(port, () => console.log(`listening to ${port}`))
