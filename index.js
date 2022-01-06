48a4d5edbeb5470099ad83886866a0adc4e8fa27
Shrikanta Mazumder <shrikanta@genuino.world>
2022-01-07 02:19:54 +0600

    add error log to console and files

--- index.js
+++ index.js
@@ -1,65 +1,92 @@
-'use strict'
+"use strict";
 
 // library function
 import express from "express";
 import expressWinston from "express-winston";
 import winston from "winston";
+import "winston-daily-rotate-file";
 import configure from "./controllers";
-import { handleErrors } from './middlewares/handleErrors';
+import { handleErrors } from "./middlewares/handleErrors";
 import connectWithDB from "./mongo";
 
-
-const app = express()
-const port = 3000
-app.use(express.json())
+const app = express();
+const port = 3000;
+app.use(express.json());
 
 const log = (msg) => console.log(msg);
 
 // process request middleware
 const processRequest = (req, res, next) => {
-    let correlationId = req.headers['x-correlation-id'];
+  let correlationId = req.headers["x-correlation-id"];
 
-    if (!correlationId) {
-        correlationId = Date.now().toString()
-        req.headers['x-correlation-id'] = correlationId
-    }
+  if (!correlationId) {
+    correlationId = Date.now().toString();
+    req.headers["x-correlation-id"] = correlationId;
+  }
 
-    res.set('x-correlation-id', correlationId)
-    return next();
-}
-app.use(processRequest)
+  res.set("x-correlation-id", correlationId);
+  return next();
+};
+app.use(processRequest);
 
-connectWithDB()
+connectWithDB();
 
 const getMessage = (req, res) => {
-    let obj = {
-        correlationId: req.headers['x-correlation-id'],
-        requestBody: req.body
-    }
-    return JSON.stringify(obj)
-}
+  let obj = {
+    correlationId: req.headers["x-correlation-id"],
+    requestBody: req.body,
+  };
+  return JSON.stringify(obj);
+};
+
+const fileInfoLogger = new winston.transports.DailyRotateFile({
+  filename: "log-info-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+const fileErrorLogger = new winston.transports.DailyRotateFile({
+  filename: "log-error-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
 
 const infoLogger = expressWinston.logger({
-    transports: [
-      new winston.transports.Console()
-    ],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json()
-    ),
-    meta: true,
-    msg: getMessage,
-  })
+  transports: [new winston.transports.Console(), fileInfoLogger,],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  msg: getMessage,
+});
 
-app.use(infoLogger);
+const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console(),
+    fileErrorLogger,
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+});
 
-configure(app)
+// app.use(infoLogger);
+
+configure(app);
+
+// handle error logger after route
+app.use(errorLogger)
 
 // register middlewares
-app.use(handleErrors)
+app.use(handleErrors);
 
-
-app.listen(port, () => console.log(`listening to ${port}`))
+app.listen(port, () => console.log(`listening to ${port}`));
 
 /**
  * 3 Layer architecture
