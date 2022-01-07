20ceb7192cf0b066edce558edbc13467f377a773
Shrikanta Mazumder <shrikanta@genuino.world>
2022-01-07 12:28:09 +0600

    keep logs on elasticsearch

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import express from "express";
 import expressWinston from "express-winston";
 import winston from "winston";
 import "winston-daily-rotate-file";
+import { ElasticsearchTransport } from 'winston-elasticsearch';
 import 'winston-mongodb';
 import configure from "./controllers";
 import { handleErrors } from "./middlewares/handleErrors";
@@ -43,26 +44,26 @@ const getMessage = (req, res) => {
 };
 
 // ALL REQUEST LOG ON FILE
-
-// const fileInfoLogger = new winston.transports.DailyRotateFile({
-//   filename: "log-info-%DATE%.log",
-//   datePattern: "YYYY-MM-DD-HH",
-//   zippedArchive: true,
-//   maxSize: "20m",
-//   maxFiles: "14d",
-// });
-
-// ALL ERROR LOG ON FILE
-const fileErrorLogger = new winston.transports.DailyRotateFile({
-  filename: "log-error-%DATE%.log",
+const fileInfoLogger = new winston.transports.DailyRotateFile({
+  filename: "log-info-%DATE%.log",
   datePattern: "YYYY-MM-DD-HH",
   zippedArchive: true,
   maxSize: "20m",
   maxFiles: "14d",
 });
 
+// Elastic Search jobs
+const esTransportOpts = {
+  level: 'info',
+  clientOpts: {node: 'http://localhost:9200'},
+  indexPrefix: 'log-track-it'
+};
+const esTransport = new ElasticsearchTransport(esTransportOpts);
+
+// Info logger
 const infoLogger = expressWinston.logger({
   transports: [new winston.transports.Console(),
+    esTransport,
     //  fileInfoLogger,
     ],
   format: winston.format.combine(
@@ -73,16 +74,28 @@ const infoLogger = expressWinston.logger({
   msg: getMessage,
 });
 
+// ALL ERROR LOG ON FILE
+const fileErrorLogger = new winston.transports.DailyRotateFile({
+  filename: "log-error-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d",
+});
+
+// log error on mongo
 const mongoErrorLogger = new winston.transports.MongoDB({
   db: uri,
   metaKey: 'meta'
 })
 
+// create error logger
 const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.Console(),
     fileErrorLogger,
     mongoErrorLogger,
+    esTransport,
   ],
   format: winston.format.combine(
     winston.format.colorize(),
